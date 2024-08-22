6beceebd57e03ddbfc41986bac998176381a9557
Christian Hansen <mail@hansen-net.de>
2024-08-22 19:02:50 +0200

    login init

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { DataService } from '../../services/data.service';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, FormsModule, InputTextModule, CardModule, PasswordModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  
+loginForm!: FormGroup;
+public email!: string;
+public password!: string;
 
+
+constructor(private data:DataService) {
+
+}
+
+ngOnInit(){
+  this.buildLoginForm();
+  this.email = this.data.email;
+}
+
+buildLoginForm() {
+  this.loginForm = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+});
+}
+
+test() {
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+    console.log('Email:', email);
+    console.log('Password:', password);
+    this.data.email = email;
+    console.log("this.data.email", this.data.email);
+    
+  }
 }
--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-start',
@@ -13,9 +14,13 @@ import { InputTextModule } from 'primeng/inputtext';
 export class StartComponent {
   public enteredEmail!: string;
 
+  constructor(public data: DataService) {
+
+  }
 
   forwardEmail() {
-    console.log("this.enteredEmail", this.enteredEmail);
+    this.data.email = this.enteredEmail;
+    console.log("this.data.email", this.data.email);
     
   }
 }
