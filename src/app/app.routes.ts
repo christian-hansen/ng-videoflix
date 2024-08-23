31de4fe304407ebc92dc403592b06fc3484cac85
Christian Hansen <mail@hansen-net.de>
2024-08-23 19:24:29 +0200

    created register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './components/start/start.component';
 import { LoginComponent } from './components/login/login.component';
+import { VideosComponent } from './components/videos/videos.component';
 
 export const routes: Routes = [
     { path: '', component: StartComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: LoginComponent },
+    { path: 'videos', component: VideosComponent },
 ];
