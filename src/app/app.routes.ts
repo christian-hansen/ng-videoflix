eb5c1762e3e43dd9d1935faa0a484cbfd31046f9
Christian Hansen <mail@hansen-net.de>
2024-08-23 19:44:08 +0200

    Sign up init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { StartComponent } from './components/start/start.component';
 import { LoginComponent } from './components/login/login.component';
 import { VideosComponent } from './components/videos/videos.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: StartComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'videos', component: VideosComponent },
 ];
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { DataService } from '../../services/data.service';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
+import { MessagesModule } from 'primeng/messages';
+import { CheckboxModule } from 'primeng/checkbox';
+import { Message } from 'primeng/api';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, FormsModule, InputTextModule, CardModule, PasswordModule, ReactiveFormsModule, MessagesModule, CheckboxModule, DividerModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
+  isLoading: boolean = false;
+  isCookieSet: boolean = false;
+  messages: Message[] = [];
+  
+  constructor(private data:DataService) {
+  
+  }
 
+  ngOnInit(){
+    this.buildRegisterForm();
+    // this.email = this.data.email;
+  }
+
+  buildRegisterForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      first_name: new FormControl('', [
+        Validators.minLength(2),
+        Validators.required,
+      ]),
+      password: new FormControl('', [Validators.required]),
+      password_repeat: new FormControl('', [Validators.required]),
+
+  });
+  }
+
+  register() {
+
+  }
+
+  resetForm() {
+    this.isLoading = false;
+  }
+
+  isPassWordsMatching(): boolean {
+    return this.registerForm.value.password === this.registerForm.value.password_repeat;
+  }
 }
