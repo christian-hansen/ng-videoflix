db5da6574a973bd536f70a8f2089f56cafd9116c
Christian Hansen <mail@hansen-net.de>
2024-09-26 10:19:54 +0200

    forgot pw component init

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,12 +1,82 @@
 import { Component } from '@angular/core';
+import { MessagesModule } from 'primeng/messages';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { Message } from 'primeng/api';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, FormsModule, ReactiveFormsModule, InputTextModule, MessagesModule, FloatLabelModule, RouterLink, RouterLinkActive],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+  forgotPasswordForm!: FormGroup;
+  messages: Message[] = [];
+  isLoading: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(){
+    this.buildForgotPasswordForm();
+          // Subscribe to form's statusChanges to log validity state
+  this.forgotPasswordForm.statusChanges.subscribe(status => {
+    console.log('Form Valid:', this.forgotPasswordForm.valid);
+//     setTimeout(() => {
+//     if (!this.forgotPasswordForm.valid) {
+
+//         this.messages = [
+//           { severity: 'info', detail: `Please enter a valid email` },
+//         ];
+//       }else {
+//         this.messages = []
+// } 
+// }, 5000);
+    });
+  }
+  
+  
+  buildForgotPasswordForm() {
+    this.forgotPasswordForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+  });
+  }
+
+  triggerForgotPasswordMail() {
+    const email = this.forgotPasswordForm.get('email')?.value
+    console.log("Password reset mail sent to", email);
+    try {
+      // let resp: any = await this.auth.loginWithUsernameAndPassword(loginFormData);
+      // this.router.navigateByUrl('');
+    } catch (e: any) {
+      this.displayErrorMessage(e)
+    }
+    
+  }
+
+  displayErrorMessage(e: any) {
+    console.log(e);
+    
+    if (e.status === 0) {
+      console.error(e)
+      this.messages = [
+        { severity: 'info', detail: `There is a problem with the server (${e.status})` },
+      ];}
+    else if (e.status === 403) {
+      this.messages = [
+        { severity: 'info', detail: `${e.error.error}` },
+      ];
+    }
+    else {
+      this.messages = [
+        { severity: 'info', detail: `${e.error.non_field_errors} (${e.status})` },
+      ];}
+    this.isLoading = false;
+    }
 
 }
